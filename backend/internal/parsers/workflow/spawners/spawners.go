3d0b5d58528aedfa10e2581c2fb280160bdb2dcd
leon <lmsakhin_1@edu.hse.ru>
2025-09-02 13:36:35 +0300

    Removed credentials package

--- backend/internal/parsers/workflow/spawners/spawners.go
+++ backend/internal/parsers/workflow/spawners/spawners.go
@@ -1,9 +1,7 @@
 package spawners
 
 import (
-	"github.com/LeonidS635/PriceChecker/backend/internal/credentials"
 	"github.com/LeonidS635/PriceChecker/backend/internal/domain"
-	"github.com/LeonidS635/PriceChecker/backend/internal/dto"
 	"github.com/LeonidS635/PriceChecker/backend/internal/parsers"
 	"github.com/LeonidS635/PriceChecker/backend/internal/parsers/portals"
 	"github.com/LeonidS635/PriceChecker/backend/internal/parsers/portals/aerobay"
@@ -14,7 +12,6 @@ import (
 	"github.com/LeonidS635/PriceChecker/backend/internal/parsers/portals/allaero"
 	"github.com/LeonidS635/PriceChecker/backend/internal/parsers/portals/boeingshop"
 	"github.com/LeonidS635/PriceChecker/backend/internal/parsers/portals/dasi"
-	"github.com/LeonidS635/PriceChecker/backend/internal/parsers/portals/excel"
 	"github.com/LeonidS635/PriceChecker/backend/internal/parsers/portals/globalaviation"
 	"github.com/LeonidS635/PriceChecker/backend/internal/parsers/portals/lasaero"
 	"github.com/LeonidS635/PriceChecker/backend/internal/parsers/portals/proponent"
@@ -61,9 +58,9 @@ func init() {
 	}
 }
 
-func RegisterExcelSpawner(path string) domain.PortalID {
-	fileID := domain.PortalID(len(Spawners) + 1)
-	Spawners[fileID] = models.NewExcelSpawner(excel.NewExcelParser, path)
-	credentials.Credentials[fileID] = dto.Credentials{} // TODO: temporary fix
-	return fileID
-}
+//func RegisterExcelSpawner(path string) domain.PortalID {
+//	fileID := domain.PortalID(len(Spawners) + 1)
+//	Spawners[fileID] = models.NewExcelSpawner(excel.NewExcelParser, path)
+//	credentials.Credentials[fileID] = dto.Credentials{} // TODO: temporary fix
+//	return fileID
+//}
